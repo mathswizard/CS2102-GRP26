ff5d891e208a0c9c608136a9a07ebab482b0c490
Samuel Wong <wongkeinwei@gmail.com>
2012-09-26 18:36:53 +0800

    Update Again

--- index.php
+++ index.php
@@ -9,17 +9,18 @@
 <body>
 <h3>FoodDB</h3>
 
+<!-- LOGIN -->
 <div id="login">
 <form>
 <h5>Member Login:</h5>
 <ul>
 <li><field>Email: </field><input type="text"><br></li>
-<li><field>Password: </field><input type="text"><br></li>
+<li><field>Password: </field><input type="password"><br></li>
 <li><button type="submit" value="Login">Log in</button></li>
 </ul>
 </form>
 </div>
-
+<!-- LOGIN END -->
 
 
 </body>
